d1fe2eb4fb6363d4a9d3645acfd4b6fab8d49ce6
github <92712468+kwhc441@users.noreply.github.com>
2022-01-24 09:12:56 +0900

    ok

--- allange/take_picture.py
+++ allange/take_picture.py
@@ -14,7 +14,8 @@ cam = cv2.VideoCapture(0)
 cam.set(3, 640) # set video width
 cam.set(4, 480) # set video height
 
-face_detector = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+face_detector = cv2.CascadeClassifier('C:\\Users\\kwhc4\\Desktop\\forclass\\project\\allange\\haarcascade_frontalface_default.xml')#xmlファイルの場所をフルパスで指定
+
 
 # For each person, enter one numeric face id
 face_id = input('\n enter user id end press <return> ==>  ')
@@ -30,7 +31,7 @@ while(True):
     time.sleep(1)
     #img = cv2.flip(img, -1) # flip video image vertically
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+    faces = face_detector.detectMultiScale(gray, 1.3, 5)#エラーの原因？
 
     for (x,y,w,h) in faces:
 
